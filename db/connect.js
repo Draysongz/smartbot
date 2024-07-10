50d0bb7faab078a8ff8ea92bacc87f7471229948
Knowledge-JO <megamindtheincredible@gmail.com>
2024-07-10 18:53:09 +0100

    update

--- db/connect.js
+++ db/connect.js
@@ -1,18 +1,17 @@
-const mongoose = require('mongoose')
-const dotenv = require('dotenv').config()
+const mongoose = require("mongoose")
+const dotenv = require("dotenv").config()
 
+const MONGO_URL = process.env.MONGO_URL || ""
 
-const MONGO_URL = process.env.MONGO_URL || ''
-
-const connectDB = async()=>{
-    try {
-        const conn = await mongoose.connect(MONGO_URL)
-    console.log('db connected successfully', conn.connection.id)
-    } catch (error) {
-        console.log(error)
-    }
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(MONGO_URL)
+    console.log("db connected successfully", conn.connection.id)
+  } catch (error) {
+    console.log(error)
+  }
 }
 
-module.exports ={
-connectDB
-} 
+module.exports = {
+  connectDB,
+}
